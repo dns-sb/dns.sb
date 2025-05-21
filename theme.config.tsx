1bce6488c3e2b8235726c1820fab2d75301459ad
Xiufeng Guo <i@m.ac>
2025-05-22 00:17:46 +0900

    Update
    Replace Twitter icon to X icon
    Add Impressum
    Year 2025

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,11 +13,11 @@ const footerLinks = [
 
 const footerContacts = [
   {
-    name: 'Twitter',
-    href: 'https://twitter.com/dns_dot_sb',
+    name: '𝕏',
+    href: 'https://x/dns_dot_sb',
     icon: (
-      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden>
-        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+      <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="currentColor" viewBox="0 0 50 50">
+  <path d="M11 4C7.134 4 4 7.134 4 11v28c0 3.866 3.134 7 7 7h28c3.866 0 7-3.134 7-7V11c0-3.866-3.134-7-7-7H11zm2.086 9h7.937l5.637 8.01L33.5 13H36l-8.211 9.613L37.914 37h-7.936l-6.54-9.293L15.5 37H13l9.309-10.896L13.086 13zm3.828 2l14.107 20h3.064L18.979 15h-3.065z"/>
       </svg>
     )
   },
@@ -85,16 +85,25 @@ const config: DocsThemeConfig = {
             </div>
             <div className="text-center px-2">
               <span className="text-sm tracking-wide">
-                Copyright &copy; 2019{' '}-{' '}<CurrentYear defaultYear={2023} />
+                Copyright &copy; 2019{' '}-{' '}<CurrentYear defaultYear={2025} />
                 <br className="md:hidden" />
                 <span className="hidden md:inline">{' '}</span>
                 <a
-                  href="https://xtom.com"
+                  href="https://xtom.com/"
                   target="_blank"
                   rel="noreferrer nooopenner"
                   className="underline"
                 >
                   xTom
+                </a>
+                {' | '}
+                <a
+                  href="https://xtom.com/impressum/"
+                  target="_blank"
+                  rel="noreferrer nooopenner"
+                  className="underline"
+                >
+                Impressum
                 </a>
                 {' '}
                 | All rights reserved.
