7d00bb65e0ea30882697a78f65ae572cba52431b
ozhb <zchcewd@gmail.com>
2025-05-24 19:16:06 +0800

    Fix bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,8 @@ import Script from 'next/script';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import AreYouUsingDnsSb from '../components/detect-dnssb';
-import {useEffect} from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const theme = localStorage.getItem('theme');
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, []);
-
   if ('statusCode' in pageProps && pageProps.statusCode === 404) {
     return (
       <Component {...pageProps} />
